cdca61eba1824a57f15ecd800639274ca74b1f48
William S. O'Dell <william.s.odell@gmail.com>
2014-08-11 20:18:38 -0600

    Fixed dates for every browser except for chrome...

--- app/models/countdown.rb
+++ app/models/countdown.rb
@@ -5,8 +5,16 @@ class Countdown < ActiveRecord::Base
 			config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
 		end
 
-		client.search("\##{hashtag}", :result_type => "recent").take(25).collect do |tweet|
-			{:name => "#{tweet.user.screen_name}", :text => "#{tweet.text}"}
+		i = 0
+
+		begin
+			client.search("\##{hashtag}", :result_type => "recent").take(25).collect do |tweet|
+				{:name => "#{tweet.user.screen_name}", :text => "#{tweet.text}"}
+			end
+		rescue Twitter::Error => error
+			i += 1
+			retry if i < 2
+			[]
 		end
 	end
 end
